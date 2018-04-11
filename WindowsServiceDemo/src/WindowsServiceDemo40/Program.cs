a0358b68a13d7698227152d24804aec53364ec07
program-pattern <program-pattern@outlook.com>
2018-04-11 11:14:34 +0800

    feat: Windows Service Demo for .net 4.0

--- WindowsServiceDemo/src/WindowsServiceDemo40/Program.cs
+++ WindowsServiceDemo/src/WindowsServiceDemo40/Program.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Topshelf;
+
 namespace PatternsCode.WindowsServiceDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            HostFactory.Run(x =>
+            {
+                x.UseNLog();
+                x.Service<ServiceRunner>();
+                x.RunAsLocalSystem();
+                x.SetDescription("A Windows Service Demo using TopShelf and Quartz");
+                x.SetDisplayName("WindowsServiceDemo");
+                x.SetServiceName("WindowsServiceDemoService");
+                x.EnablePauseAndContinue();
+            });
         }
     }
 }
---
+++ WindowsServiceDemo/src/WindowsServiceDemo40/TestJob.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Quartz;
+using NLog;
+
+namespace PatternsCode.WindowsServiceDemo
+{
+    public sealed class TestJob : IJob
+    {
+        Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+        public void Execute(IJobExecutionContext context)
+        {
+            _logger.Info("Test Job");
+        }
+    }
+}
